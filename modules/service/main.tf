b66d3457c90da797af5ded1a04f5fac915c6f2d5
Rodrigo Torres <rlinstorres@gmail.com>
2024-02-27 15:06:05 +0100

    chore: Setting IAM role in ignore changes

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -391,6 +391,7 @@ resource "aws_ecs_service" "ignore_task_definition" {
     ignore_changes = [
       desired_count, # Always ignored
       task_definition,
+      iam-role # Ignoring changes in IAM role when using both ALBs
     ]
   }
 }
